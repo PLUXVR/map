cf8b42b30aa792926e21f6f74746c602b72bb2b7
PLUXVR <pluxar@bk.ru>
2023-12-05 21:18:09 +0300

    Object Model

---
+++ lib/models/object_post_model.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+// Класс для хранения данных об объекте
+class ObjectModel {
+  final String objectName;
+  final double latitude;
+  final double longitude;
+  final String objectDescription;
+  final File image;
+// Конструктор класса, все поля обязательные
+  ObjectModel({
+    required this.image,
+    required this.longitude,
+    required this.latitude,
+    required this.objectDescription,
+    required this.objectName,
+  });
+}
--- lib/screens/add_object.dart
+++ lib/screens/add_object.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_map_test/models/object_post_model.dart';
 import 'package:latlong2/latlong.dart';
 
 class AddObjectScreen extends StatefulWidget {
@@ -13,41 +14,67 @@ class AddObjectScreen extends StatefulWidget {
 }
 
 class _AddObjectScreenState extends State<AddObjectScreen> {
+  String? name;
+  String? description;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text("Добавить объект"),
-        // leading: ,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 3,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                image: DecorationImage(
-                  image: FileImage(widget.image),
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  image: DecorationImage(
+                    image: FileImage(widget.image),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-              child:
-                  Text('${widget.latLng.latitude} ${widget.latLng.latitude}'),
-            ),
-            TextField(),
-            TextField(),
-            // ElevatedButton(
-            //     onPressed: () => Navigator.of(context).pop(),
-            //     child: const Text('Pop!'))
-          ],
+              const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text('Координаты объекта: '),
+              ),
+              Text('Ширина: ${widget.latLng.latitude}'),
+              Text('Долгота: ${widget.latLng.longitude}'),
+              TextField(onChanged: (value) {
+                name = value;
+              }),
+              TextField(onChanged: (value) {
+                description = value;
+              }),
+              // ElevatedButton(
+              //     onPressed: () => Navigator.of(context).pop(),
+              //     child: const Text('Pop!'))
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          // Сохраняем объекты после создания и показываем на карте
+
+          // if (description != null && name != null) {
+          // if description != "" && name != ""}
+
+          final ObjectModel objectModel = ObjectModel(
+              image: widget.image,
+              longitude: widget.latLng.longitude,
+              latitude: widget.latLng.latitude,
+              objectDescription: description ?? 'Empty',
+              objectName: name ?? 'Empty');
+
+          // Pop экрана после нажатия на кнопку
+          Navigator.of(context).pop();
+        },
         child: const Icon(Icons.check, size: 30),
       ),
     );
